af6c41ec96d93ec759cc1bcb1f4a98a658dd0861
Somondas <somondas067@gmail.com>
2024-08-17 21:26:38 +0530

    Add Member modal, done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import AppLayout from "../components/AppLayout";
 
 const Home = () => {
+  // ! The site is not for small screens
   return (
     <div className="w-full  h-full  p-4 ">
       <div className="w-full h-full border-2 rounded-lg">
         <h1 className="font-semibold text-4xl capitalize m-4">
           Welcome, John Doe
         </h1>
+        <p></p>
       </div>
     </div>
   );
